b3ebd9b243c4a29df0b718cde9f10a031d4edbbb
Okpainmo <okpainmoandrew@gmail.com>
2025-08-05 06:14:32 +0100

    chore: current implementations - added more updates including prettier and solhint

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# ethersjs-project-helper
+# ethersjs-project-helper.
+
 An Ethers helper project - demonstrating Solidity smart contract deployment and interaction - with only the Ethers Javascript Library. A free gem-resource for bootstrapping/starting similar projects.
---
+++ src/contract-interactions.ts
@@ -0,0 +1,56 @@
+import fs from "fs";
+import { ethers } from "ethers";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function contractInteractions() {
+    const RPC_URL = process.env.RPC_URL || "http://127.0.0.1:7545";
+    const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+    let provider = new ethers.JsonRpcProvider(RPC_URL);
+    console.log("block number", await provider.getBlockNumber());
+
+    // let signer = new ethers.Wallet(PRIVATE_KEY as string, provider);
+    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+    let signer = ethers.Wallet.fromEncryptedJsonSync(
+        encryptedJson,
+        process.env.PRIVATE_KEY_PASSWORD as string,
+    ).connect(provider);
+
+    const abi = JSON.parse(
+        fs.readFileSync(
+            "./contracts/compilation-details/_contracts_Storage_sol_SimpleStoragePracticeContract.abi",
+            "utf8",
+        ),
+    );
+    // console.log('contract abi', abi);
+
+    // const binary = fs
+    //   .readFileSync(
+    //     './contracts/compilation-details/_contracts_Storage_sol_SimpleStoragePracticeContract.bin',
+    //     'utf8'
+    //   )
+    //   .trim();
+
+    const contractInstance = new ethers.Contract(
+        "0x58348A98819bc1B74Df7AF4bB62b3A77fE504f21",
+        abi,
+        provider,
+    );
+
+    /* the below contract interactions might work differently on a testnet as it would on a mainnet - if called together like was done below.
+      Call the contracts separately instead - in different functions/processes*/
+    const currentNumber = await contractInstance?.handleRetrieve();
+    console.log(`currentNumber Number: ${currentNumber}`);
+
+    // @ts-ignore
+    await contractInstance?.connect(signer).handleStore(90);
+    console.log("Updating number...");
+
+    // @ts-ignore
+    const newNumber = await contractInstance?.handleRetrieve();
+    console.log(`New Number: ${newNumber}`);
+}
+
+contractInteractions();
